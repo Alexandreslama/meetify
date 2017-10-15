dc9099ed84c6df3778b69a8a9b88082b27226186
Alexandreslama <alexandre.slama@hetic.net>
2017-10-15 17:39:43 +0200

    Adding the Profil updater

---
+++ wp-content/themes/meetify/inc/create_film_post-types.php
@@ -0,0 +1,64 @@
+<?php 
+function ajout_custom_type_film() {
+	$post_type = "film";
+$labels = array(
+        'name'               => 'Profils',
+        'singular_name'      => 'Profil',
+        'all_items'          => 'Tous les Profils',
+        'add_new'            => 'Ajouter un Profil',
+        'add_new_item'       => 'Ajouter un nouveau Profil',
+        'edit_item'          => "Modifier le Profil",
+        'new_item'           => 'Nouveau Profil',
+        'view_item'          => "Voir le Profil",
+        'search_items'       => 'Chercher un Profil',
+        'not_found'          => 'Pas de résultat',
+        'not_found_in_trash' => 'Pas de résultat',
+        'parent_item_colon'  => 'Film parent:',
+        'menu_name'          => 'Profils',
+    );
+
+    $args = array(
+        'labels'              => $labels,
+        'hierarchical'        => false,
+        'supports'            => array( 'title','thumbnail','editor', 'excerpt', 'trackbacks', 'comments', 'revisions', 'page-attributes', 'post-formats' ),
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-admin-users',
+        'show_in_nav_menus'   => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'has_archive'         => false,
+        'query_var'           => true,
+        'can_export'          => true,
+        'rewrite'             => array( 'slug' => $post_type )
+    );
+
+    register_post_type($post_type, $args );
+
+    $taxonomy = "genre";
+    $object_type = array("Profil");
+    $args = array(
+          'label' => __( 'Genre' ),
+          'rewrite' => array( 'slug' => 'genre' ),
+          'hierarchical' => true,
+      );
+    register_taxonomy( $taxonomy, $object_type, $args );
+       
+    $taxonomy = "region";
+    $object_type = array("Profil");
+    $args = array(
+          'label' => __( 'Ville' ),
+          'rewrite' => array( 'slug' => 'ville' ),
+          'hierarchical' => false,
+      );
+
+    register_taxonomy( $taxonomy, $object_type, $args );
+
+
+
+
+}
+
+add_action ('init', 'ajout_custom_type_film');
---
+++ wp-content/themes/meetify/inc/styles_and_scripts.php
@@ -0,0 +1,33 @@
+<?php 
+function ajout_scripts() {
+
+// enregistrement d'un nouveau script
+wp_register_script('bootstrap_script', JS_URL. '/bootstrap.min.js', array('jquery'),'1.1', true);
+wp_enqueue_script('bootstrap_script');
+
+// enregistrement d'un nouveau script
+wp_register_script('main_script', JS_URL. '/main.js', array('jquery'),'1.1', true);
+wp_enqueue_script('main_script');
+
+
+
+
+// enregistrement d'un nouveau style
+wp_register_style( 'googlefont_style', 'https://fonts.googleapis.com/css?family=Montserrat|Roboto:300,400,500,700,900' );
+wp_enqueue_style( 'googlefont_style' );
+
+// enregistrement d'un nouveau style
+wp_register_style( 'bootstrap_style', CSS_URL. '/bootstrap.min.css' );
+wp_enqueue_style( 'bootstrap_style' );
+
+
+
+// enregistrement d'un nouveau style
+wp_register_style( 'main_style', CSS_URL. '/main.css' );
+
+// appel du style dans la page
+wp_enqueue_style( 'main_style' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
